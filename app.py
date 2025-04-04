f85660a85d9756c0d72af9b25f6868fb65365224
akash01-hub <mauryaakashkumar458@gmail.com>
2025-04-04 18:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Aviator Crash Game API is running!"
+    return render_template("index.html")  # serve frontend
 
 @app.route('/crash', methods=['GET'])
 def crash():
